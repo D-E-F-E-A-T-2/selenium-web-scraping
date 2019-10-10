12f9df306dc135cf35d612ff0a2679484846425e
Kush Choudhary <kushchoudhary8@gmail.com>
2019-10-10 19:14:05 +0530

    https://thehackernews.com/

---
+++ daily_hack_news.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+## --Getting only headings of news from www.thehackernews.com--
+## Made by Kushal 18BCE0557
+
+
+from time import sleep
+from bs4 import BeautifulSoup as soup #BeautifulSoup
+sleep(5)
+from urllib.request import urlopen as u_req #urllib.request
+my_url = "https://thehackernews.com"
+u_client = u_req(my_url)
+page_html = u_client.read()
+u_client.close()
+page_soup = soup(page_html, "html.parser")
+container = page_soup.findAll("div", {"class":"clear home-right"})
+for getter in container:
+    print(getter.h2.text)
+    print()
+sleep(5)
